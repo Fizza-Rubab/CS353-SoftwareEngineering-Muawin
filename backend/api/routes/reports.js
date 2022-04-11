8a349b10ba7660032a7e810d894e499257b51463
RuhamaN <56465221+RuhamaN@users.noreply.github.com>
2022-04-12 01:29:18 +0500

    customized pdf generation and grades handling

--- backend/api/routes/reports.js
+++ backend/api/routes/reports.js
@@ -1,3 +1,6 @@
+//import { getStudents } from "../functions.js";
+const data = require("../functions.js");
+
 const express = require('express');
 const router = express.Router();
 const PDFDocument = require('pdfkit-table');
@@ -9,64 +12,100 @@ router.get('/', (req, res, next) => {
     const termId = req.query.termId;
     console.log(classId,  termId);
 
-    // Document Generation Code
+    students = data.getStudents(classId, termId);
+    courses = data.getCourses(classId, termId);
+
+    st = Object.keys(students.Students).length;
+    cr = Object.keys(courses.Courses).length;
+    const Grades = {};
+
+    for (let i = 1; i <= st; i++) {
+      var name = students.Students[i].Name;
+      Grades[name] = {};
+      for (let j = 1; j<= cr; j++) {
+        grades = data.getGrades(students.Students[i].Id, j);
+        course = courses.Courses[j];
+        Grades[name][course] = {}
+        Grades[name][course] =  grades;
+      }
+    }
+
+    for (let i = 1; i <= st; i++){
+      var name = students.Students[i].Name;
+        
+    }
+
     const doc = new PDFDocument();
 
-    // Pipe its output somewhere, like to a file or HTTP response
-    // See below for browser usage
-    doc.pipe(fs.createWriteStream('output.pdf'));
-
-    // Embed a font, set the font size, and render some text
-    doc
-    .fontSize(25)
-    .text('Hey Fizza Testing here', 100, 100);
-
-
-    // Add another page
-    doc
-    .addPage()
-    .fontSize(25)
-    .text('Here is some vector graphics...', 100, 100);
-
-    const table = {
+      // Pipe its output somewhere, like to a file or HTTP response
+      // See below for browser usage
+      doc.pipe(fs.createWriteStream('G:\\SEM 6\\SOFTWARE ENGINEERING\\project\\Muawin-SE\\backend\\reports\\output.pdf'));
+      const table = {
         title: "Title",
         subtitle: "Subtitle",
-        headers: ["Country", "Conversion rate", "Trend"],
+        headers: [
+          { label:"Name", property: 'name', width: 60, renderer: null },
+          { label:"Description", property: 'description', width: 150, renderer: null }, 
+          { label:"Price 1", property: 'price1', width: 100, renderer: null }, 
+          { label:"Price 2", property: 'price2', width: 100, renderer: null }, 
+          { label:"Price 3", property: 'price3', width: 80, renderer: null }, 
+          { label:"Price 4", property: 'price4', width: 43, 
+            renderer: (value, indexColumn, indexRow, row) => { return `U$ ${Number(value).toFixed(2)}` } 
+          },
+        ],
+        // complex data
+        datas: [
+          { 
+            name: 'Name 1', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ante in laoreet egestas. ', 
+            price1: '$1', 
+            price3: '$ 3', 
+            price2: '$2', 
+            price4: '4', 
+          },
+          { 
+            options: { fontSize: 10, separation: true},
+            name: 'bold:Name 2', 
+            description: 'bold:Lorem ipsum dolor.', 
+            price1: 'bold:$1', 
+            price3: { 
+              label: 'PRICE $3', options: { fontSize: 12 } 
+            }, 
+            price2: '$2', 
+            price4: '4', 
+          },
+          // {...},
+        ],
+        // simeple data
         rows: [
-          ["Switzerland", "12%", "+1.12%"],
-          ["France", "67%", "-0.98%"],
-          ["England", "33%", "+4.44%"],
+          [
+            "Apple",
+            "Nullam ut facilisis mi. Nunc dignissim ex ac vulputate facilisis.",
+            "$ 105,99",
+            "$ 105,99",
+            "$ 105,99",
+            "105.99",
+          ],
+          // [...],
         ],
       };
-    doc.table( table, { 
-    // A4 595.28 x 841.89 (portrait) (about width sizes)
-    width: 300,
-    //columnsSize: [ 200, 100, 100 ],
-    }); 
-
-    // Apply some transforms and render an SVG path with the 'even-odd' fill rule
-    doc
-    .scale(0.6)
-    .translate(470, -380)
-    .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
-    .fill('red', 'even-odd')
-    .restore();
-
-    // Add some text with annotations
-    doc
-    .addPage()
-    .fillColor('blue')
-    .text('Here is a link!', 100, 100)
-    .underline(100, 100, 160, 27, { color: '#0000FF' })
-    .link(100, 100, 160, 27, 'http://google.com/');
-
-    // Finalize PDF file
+    
+      doc.table(table, {
+        prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8),
+        prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+          doc.font("Helvetica").fontSize(8);
+          indexColumn === 0 && doc.addBackground(rectRow, 'blue', 0.15);
+        },
+          width:20,
+          height:40,
+          x:30,
+          y:40
+      });
+    
     doc.end();
-    res.status(200).json({
-        message: 'following parameters will be used to get term grades for students from database',
-        classId: classId,
-        termId: termId
-    });
+ 
+
+    res.status(200).json(Grades);
 
 });
 
