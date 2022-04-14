9b534d0b210f9d2bd42005aac56487011b00e84d
Fizza-Rubab <fr06161@st.habib.edu.pk>
2022-04-14 12:25:59 +0500

    added  to exports

--- backend/api/functions.js
+++ backend/api/functions.js
@@ -33,4 +33,4 @@ function getGrades(studentID, courseID){
 
 
 
-module.exports = { getStudents, getCourses, getGrades }; 
+module.exports = { getStudents, getCourses, getGrades, getAttendanceToday}; 
